0508db4ffc327c3751e28dbf462e19280df5c761
NicolasCl86 <n.d.kleovoulou@gmail.com>
2024-11-10 13:50:21 +0200

    Update SEO to include FB tags in header

--- components/SEO.js
+++ components/SEO.js
@@ -6,6 +6,7 @@ export default function SEO({ title, description }) {
       <title>{title}</title>
       <meta name="description" content={description} />
       <meta property="og:title" content={title} />
+      <meta name="facebook-domain-verification" content="ztvy7x6f4c9hudlpaysrim812ntbsm" />
     </Head>
   );
 }
